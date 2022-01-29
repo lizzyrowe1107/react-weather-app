3163cedbc897f1f177c1f9219056a3af2367bb99
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-01-29 11:01:54 -0800

    React Setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import WeatherSearchCity from './WeatherSearchCity';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1>
+          My Weather App
+        </h1>
+        <WeatherSearchCity city="Paris"/>
+        
       </header>
     </div>
   );
---
+++ src/WeatherSearchCity.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+export default function WeatherSearchCity(props) {
+
+    function handleResponse(response) {
+        alert(`The weather in ${response.data.name} is ${response.data.main.temp} C`)
+    }
+    
+    let apiKey = "bad7d22d20acf170eb966f0d0727a2fc";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return (
+        <h1>
+            Hello from weather app
+        </h1>
+    )
+
+}
