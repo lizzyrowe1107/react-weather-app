15f386573b0b569c609c20352e7c843a34db8a07
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-01-30 14:03:04 -0800

    Added GitHub Link and Personal Website Link

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import './App.css';
 import WeatherSearchCity from './WeatherSearchCity';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+      
 
         <h1>
           My Weather App
         </h1>
         <WeatherSearchCity city="Paris"/>
+        <footer>
+          This project is coded by {" "}
+          
+          <a href="https://itsjustliz.org" target="_blank">Liz Rowe</a> 
+          
+          {" "} and is {" "}
 
-        <a href="https://github.com/lizzyrowe1107/react-weather-app">
-          GitHub Link
+        <a href="https://github.com/lizzyrowe1107/react-weather-app" target="_blank">
+         open-sourced on GitHub
         </a>
         
-      </header>
+        </footer>
+      
     </div>
   );
 }
 
-export default App;
+
