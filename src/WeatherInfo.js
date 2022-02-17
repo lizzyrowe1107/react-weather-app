fbe63b77c0d57231dbb4732bc00a438cc7ce1537
Liz Rowe <83372467+lizzyrowe1107@users.noreply.github.com>
2022-02-17 07:32:45 -0800

    Reduce Size of Icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -21,7 +21,7 @@ export default function WeatherInfo(props) {
                     
                     <span className="float-left">
 
-                        <WeatherIcon code={props.data.icon} size={52}/>
+                        <WeatherIcon code={props.data.icon} size={44}/>
                     </span>
                 
                     <WeatherTemperature celsius={props.data.temperature}/>
